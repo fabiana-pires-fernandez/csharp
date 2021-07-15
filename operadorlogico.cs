34a8d949b0272edd77d9b76709416b3f5d7f8ef8
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-15 19:03:11 -0300

    Exercicio

---
+++ operadorlogico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace operadorlogico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float media, frequencia;
+            Console.WriteLine("Informe sua media: ");
+
+            media = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe sua frequência: ");
+            frequencia = float.Parse(Console.ReadLine());
+
+            if(media>=7 && frequencia>=75) {
+                Console.WriteLine("Você está aprovado!");
+            }
+        }
+    }
+}
