7a332adefc5016a60b1929cbe3489fa9a5ae33b7
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-15 17:50:11 -0300

    Exercicios

---
+++ areatriangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace areat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float baset, alturat, areat;
+            
+            Console.WriteLine("Informe a base do triângulo: (em m)");
+            baset = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a altura do triângulo: (em m).");
+            alturat = float.Parse(Console.ReadLine());
+
+            areat = (baset * alturat) / 2;
+            Console.WriteLine("A área do triângulo é {0}m.", areat);
+
+        }
+    }
+}
