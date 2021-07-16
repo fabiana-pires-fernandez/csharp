6265bdf1af401fc2fc7a76dd7fe2a9e2e5dc183a
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-16 17:29:54 -0300

    Exercicios

---
+++ operadorrelacional.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace operadorrelacional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor;
+            int ano;
+
+            Console.WriteLine("Informe a cor desejada para o automóvel: ");
+            cor = Console.ReadLine();
+
+            Console.WriteLine("Informe o ano inicial: ");
+            ano = int.Parse(Console.ReadLine());
+
+            if((cor=="preto" || cor=="cinza") && ano>=2016)
+                {
+                    Console.WriteLine("Automóvel aprovado!");
+                } else {
+                    Console.WriteLine("Automóvel reprovado!");
+                }
+
+        }
+    }
+}
