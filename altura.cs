06f22a333bfe55824b04b27dc9e13041a1a703ce
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-16 17:41:41 -0300

    Exercicios

---
+++ altura.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace altobaixo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+
+            Console.WriteLine("Informe a sua altura: [use . e não ,]");
+            altura = float.Parse(Console.ReadLine());
+
+            if (altura>=1.80){
+                Console.WriteLine("Alto");
+            } else {
+                Console.WriteLine("Baixo");
+            }
+        }
+    }
+}
