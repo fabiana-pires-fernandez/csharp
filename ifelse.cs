f3ab0d13f7f3eec509aa2c8399b95185e9d65e6b
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-16 17:36:09 -0300

    exericios

---
+++ ifelse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float media;
+
+            Console.WriteLine("Informe sua média: ");
+            media = float.Parse(Console.ReadLine());
+
+            if(media>=7){
+                Console.WriteLine("Você está aprovado!");
+            } else {
+                Console.WriteLine("Você está reprovado!");
+            }
+        }
+    }
+}
