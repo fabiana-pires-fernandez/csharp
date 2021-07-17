f86f6b8e07b9ea9896585de7838d947369044545
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-17 10:10:08 -0300

    Exercicios

---
+++ sinal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+
+            Console.WriteLine("Qual é a cor [1 - Verde; 2 - Amarelo; 3 - Vermelho]?");
+            op = int.Parse(Console.ReadLine());
+
+            switch(op) {
+
+                case 1:
+                    Console.WriteLine("Siga em frente!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Atenção!");
+                    break;
+                
+                case 3:
+                    Console.WriteLine("Pare!");
+                    break;
+
+                default:
+                    Console.WriteLine("Operação inválida!");
+                    break;
+            }
+
+        }
+    }
+}
