1cf046fe407cd9b214e91be18f9b858fdc39c5f2
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-17 10:48:01 -0300

    Exercicios

---
+++ maioridade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace maioridade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento, idade;
+
+            Console.WriteLine("Digite o ano de nascimento: [Ex: 1990]");
+            anoNascimento = int.Parse(Console.ReadLine());
+            idade = DateTime.Now.Year - anoNascimento; //ano atual - ano informado
+
+            if(idade >= 18) {
+                Console.WriteLine("Maior de idade");
+            } else {
+                Console.WriteLine("Menor de idade");
+            }
+        }
+    }
+}
