6274a7259ccaa4ebbc0854d12bb80fda46f1b128
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-17 11:25:21 -0300

    Exercicios

---
+++ mediasalario2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mediasalario2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, soma=0, media;
+            int cont;
+
+            for(cont=1;cont<=5;cont++) {
+                Console.WriteLine("Informe o valor do salário: R$");
+                salario = float.Parse(Console.ReadLine());
+                soma = soma + salario;
+            }
+
+            media =  soma / cont;
+            Console.WriteLine("O valor médio dos salários é: R${0:N}.", media);
+        }
+    }
+}
