848db07666d041efa031e95747a9908de3246c73
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-17 11:07:56 -0300

    Exercicios

---
+++ mediasalario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace mediasalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario, soma=0, media;
+
+            Console.WriteLine("Informe o valor do salário: R$");
+            salario = float.Parse(Console.ReadLine());
+            soma = soma + salario;
+
+            Console.WriteLine("Informe o valor do salário: R$");
+            salario = float.Parse(Console.ReadLine());
+            soma = soma + salario;
+
+            Console.WriteLine("Informe o valor do salário: R$");
+            salario = float.Parse(Console.ReadLine());
+            soma = soma + salario;
+
+            Console.WriteLine("Informe o valor do salário: R$");
+            salario = float.Parse(Console.ReadLine());
+            soma = soma + salario;
+
+            Console.WriteLine("Informe o valor do salário: R$");
+            salario = float.Parse(Console.ReadLine());
+            soma = soma + salario;
+
+            media = soma / 5;
+            Console.WriteLine("O valor médio dos salários é : R${0:N}.", media);
+        }
+    }
+}
