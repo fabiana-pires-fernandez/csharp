b3f8a95d73c3b28744f9651eedcc0f3d0a64dece
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-19 22:13:13 -0300

    Exercicios

---
+++ fatorial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int fatorial = 1;
+            for (int n = 1; n <= 5; n++) {
+                fatorial*=n;
+            }
+                     
+            Console.WriteLine("5x4x3x2x1= "+fatorial); 
+        
+        }
+    }
+}
+
