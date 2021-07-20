5ca49a44a647d037536be60c0b7a33fdd2b35fd5
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-19 22:34:38 -0300

    Exercicios

---
+++ razao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace razao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num2, cont=0, razao;
+
+            Console.WriteLine("Informe o primeiro número: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            razao = num2 - num;
+
+            while(cont<10){
+                Console.WriteLine(" "+num);
+                num = num + razao;
+                cont++;
+            }
+        }
+    }
+}
