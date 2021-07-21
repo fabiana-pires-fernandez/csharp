9e42e760b4a004f7eecf9e3bcbd2ceed9f21c9fd
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-21 20:16:52 -0300

    Exercicios

---
+++ teste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i=5, n=5, n2=-1;
+
+            while(n2!=5) {
+                n2 = Math.Pow(n, i);
+                Console.WriteLine(" {0} ", n2);
+                if(n2<500)
+                    Console.WriteLine(" X \n");
+                else
+                    Console.WriteLine(" Y \n");
+                
+                i--;
+                n++;
+
+                if(i==0)
+                    n2=5;
+            }
+        }
+    }
+}
