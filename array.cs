d9eb3293192d83853c9132062d832ddb8c5cd706
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-21 20:27:17 -0300

    Exercicios

---
+++ array.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] contatos = new string[5];
+
+            contatos[0] = "Maria";
+            contatos[1] = "João";
+            contatos[2] = "Arthur";
+            contatos[3] = "Fernando";
+            contatos[4] = "Júlia";
+
+            for(int i = 0; i < contatos.Length; i++) {
+                Console.WriteLine("Contato " + (i +1) + " : " + contatos[i]);
+            }
+        }
+    }
+}
