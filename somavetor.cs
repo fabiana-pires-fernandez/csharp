b6c115c10777ab7812c11193bb658efffe255f7e
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-22 17:46:41 -0300

    Exercicios

---
+++ somavetor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace somavetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros;
+            int quantidade, soma;
+
+            Console.WriteLine("Informe quantos número iremos somar: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            numeros = new int[quantidade];
+            soma = 0;
+
+            for(int i = 0; i < quantidade; i++) {
+                Console.WriteLine("Digite um número: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+
+                soma = soma + numeros[i];
+             }
+            Console.WriteLine("A soma dos números é {0}.", soma);
+        }
+    }
+}
