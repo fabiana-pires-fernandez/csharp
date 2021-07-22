1dabfff7ec296feb35cbab9b47c43dea38e3a5b7
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-22 20:07:51 -0300

    Exercicios

---
+++ exercicio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] celular = {"Xiaomi", "LG", "Samsung"};
+
+            foreach(string i in celular) {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Fim!");
+        }
+    }
+}
