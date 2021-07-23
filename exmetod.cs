5145eb735faec698fbd5c8e567368e422df4f286
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-23 18:19:37 -0300

    Exercicios

---
+++ exmetod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exmetod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+           string nome = Console.ReadLine();
+           Console.WriteLine($"OLá {nome}");
+           BoasVindas();
+        }
+
+        static void BoasVindas()
+        {
+            if(DateTime.Now.Hour >= 5 && DateTime.Now.Hour < 18) {
+                Console.WriteLine("Tenha um bom dia!");
+            } else {
+                Console.WriteLine("Tenha uma boa noite!");
+            }
+        }
+    }
+}
