8c5290162b6ac85f0effda507758dfc0f771795c
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-29 18:08:10 -0300

    Exercicios

---
+++ soma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace somar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Somar(62, 24);
+        }
+        
+        static void Somar(int num1, int num2) 
+        {
+            int soma;
+            soma = num1 + num2;
+            Console.WriteLine("Resultado da soma " + soma);
+        }
+
+        
+    }
+}
---
+++ somar2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace somar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = Somar(10, 7); // atribuindo a variável soma o retorno de Somar
+            double media1 = soma / 2.0;
+            Console.WriteLine(media1); // imprime 8.5
+
+            // usando direto na expressão o retorno de Somar
+
+            double media2 = Somar(6, 9) / 2.0;
+            Console.WriteLine(media2); // imprime 7.5
+        }
+
+        static int Somar(int num1, int num2)
+        {
+            int soma;
+            soma = num1 + num2;
+            return soma;
+        }
+            
+    }
+}
