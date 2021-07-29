116f8a15d79f90761644298d50bf7dd9326d1c69
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-29 19:42:33 -0300

    Exercicios

---
+++ triangulo2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace triangulo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double retangulo = CalculaAreaRetangulo(10, 5);
+            Console.WriteLine($"Área do retângulo: {retangulo}");
+
+            double triangulo = CalculaAreaTriangulo(3, 4);
+            Console.WriteLine($"Área do triângulo: {triangulo}");
+        
+        }
+
+        static double CalculaAreaRetangulo(double b, double h)
+        {
+            double area = b * h;
+            return area;
+        }
+
+        static double CalculaAreaTriangulo(double b, double h)
+        {
+            double area = CalculaAreaRetangulo(b, h);
+            
+            area = area / 2;
+            return area;
+        }
+    }
+}
