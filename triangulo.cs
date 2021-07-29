6d9ddc6159914ff0df538bd8cab6167d2c332853
Fabi Pires <fabi.nandez@hotmail.com>
2021-07-29 18:27:06 -0300

    Exercicios

---
+++ triangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cateto1, cateto2, hipotenusa;
+            string continua;
+
+            do {
+                Console.WriteLine("Informe cateto oposto: ");
+                cateto1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe cateto adjacente: ");
+                cateto2 = double.Parse(Console.ReadLine());
+
+                hipotenusa = Math.Sqrt(Quadrado(cateto1) + Quadrado(cateto2));
+                Console.WriteLine("Hipotenusa: " + hipotenusa);
+
+                Console.Write("Deseja continuar? [s/n] ");
+                continua = Console.ReadLine();
+
+            } while(continua != "n");
+        }
+        static double Quadrado (double numero)
+        {
+            double quadrado = numero * numero;
+            return quadrado;
+        }
+    }
+}
