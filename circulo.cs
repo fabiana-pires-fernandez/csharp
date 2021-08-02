376f09c4dec505b10c642ecca3742ce36bd2efdf
Fabi Pires <fabi.nandez@hotmail.com>
2021-08-02 16:25:52 -0300

    Exercicios

---
+++ circulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace circulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double pi = 3.14;
+
+            Console.WriteLine("Digite o raio do círculo: [mm]");
+            r = double.Parse(Console.ReadLine());
+
+            double circulo = CalculaAreaCirculo(pi, r);
+            Console.WriteLine($"Área do círculo: {circulo}");
+        }
+
+        static double CalculaAreaCirculo(double pi, double r)
+        {
+            
+            double area = pi * (r*r);
+            return area;
+        }
+        
+    }
+}
